858dbf7f4618d44435bd17a9df534f43a0909b3e
Sam Avento <eazomc@gmail.com>
2024-07-03 19:22:49 +0700

    Implemented Django Module

---
+++ django_project/underwater_image_enhancement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UnderwaterImageEnhancementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'underwater_image_enhancement'
